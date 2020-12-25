3a9898b306c4801a81238646bcacbfebb49dbf0e
wuchiwo <wuchiwo@gmail.com>
2020-12-25 17:54:31 +0800

    .

--- lstm/config.py
+++ lstm/config.py
@@ -2,7 +2,6 @@ stock = 'Equities_27'
 data_file = './data/' + stock +'.csv'
 predict_file = './predict/' + stock + '.csv'
 model_dir = './lstm/model/' + stock + '/'
-
 feature_keys = [
     'Last Trade',
     'Volume',
@@ -11,26 +10,10 @@ feature_keys = [
     'MA20',
     'VOL'
 ]
-
-colors = [
-    "blue",
-    "orange",
-    "green",
-    "red",
-    "purple",
-    "brown",
-    "pink",
-    "gray",
-    "olive",
-    "cyan",
-]   
-
 predict_key = 'Last Trade'
 date_key = 'Time'
-date_format = '%Y/%m/%d %H:%M'
-
 split_fraction = 0.85
 past = 798
 future = 60
-epochs = 100 #100 #500
+epochs = 100
 batch_size = 512
--- util/data.py
+++ util/data.py
@@ -1,15 +1,19 @@
 import pandas as pd
 from pandas.core.frame import DataFrame
-import lstm.config as cfg
 from datetime import datetime as dt
 import matplotlib.pyplot as plt
 import numpy as np
  
+stocks = ['27', '200', '880', '2282']
+date_key = 'Time'
+price_key = 'Last Trade'
+date_format = '%Y/%m/%d %H:%M'
+mei_file = 'data/raw/mei.csv'
+stock_file = 'data/raw/Equities_%s.csv'
+train_file = 'data/train/Equities_%s.csv'
+
 def get_daily_return(file):
-    date_key = 'Time'
-    price_key = 'Last Trade'
-    date_parser = lambda x: dt.strptime(x, cfg.date_format)
-    data = pd.read_csv(file, parse_dates=[date_key], date_parser=date_parser)
+    data = read_data(file)
     starting_price = data.groupby(data[date_key].dt.date).first().drop(columns=[date_key]).reset_index(level=[date_key])
     ending_price = data.groupby(data[date_key].dt.date).last().drop(columns=[date_key]).reset_index(level=[date_key])
     daily_return = pd.DataFrame(columns = ['Time', 'Starting Price', 'Ending Price', 'Return'])
@@ -20,13 +24,12 @@ def get_daily_return(file):
     return daily_return
 
 def get_beta_difference():
-    market_return = get_daily_return(file='data/mei.csv')
+    market_return = get_daily_return(file=mei_file)
     positive_market_return = market_return[market_return.Return > 0]
     negative_market_return = market_return[market_return.Return < 0]
-    equities = ['27', '200', '880', '2282']
     result = pd.DataFrame(columns = ['Stock', 'Beta Difference'])
-    for equity in equities:
-        stock_return = get_daily_return(file='data/Equities_%s.csv' % equity)      
+    for stock in stocks:
+        stock_return = get_daily_return(file=stock_file % stock)      
         downside_stock_return = stock_return[stock_return.Time.isin(negative_market_return.Time)]
         downside_stock_return_values = downside_stock_return['Return'].values
         upside_stock_return = stock_return[stock_return.Time.isin(positive_market_return.Time)]
@@ -36,29 +39,34 @@ def get_beta_difference():
         downside_beta = np.cov(downside_stock_return_values, negative_market_return_values) / np.var(negative_market_return_values)
         upside_beta = np.cov(upside_stock_return_values, positive_market_return_values) / np.var(positive_market_return_values)
         beta_difference = (downside_beta - upside_beta)[0][1]
-        result = result.append({'Stock': equity, 'Beta Difference': beta_difference}, ignore_index=True)
+        result = result.append({'Stock': stock, 'Beta Difference': beta_difference}, ignore_index=True)
     return result
 
 def data_preprocessing():
-    date_parser = lambda x: dt.strptime(x, cfg.date_format)
-    data = pd.read_csv(cfg.data_file, parse_dates=['Time'], date_parser=date_parser)
-    data['MA5'] = data['Last Trade'].rolling(window=5).mean().fillna(0)
-    data['MA10'] = data['Last Trade'].rolling(window=10).mean().fillna(0)
-    data['MA20'] = data['Last Trade'].rolling(window=20).mean().fillna(0)
-    data['VOL'] = data['Last Trade'].rolling(window=10).std().fillna(0)
-    data['EMA20'] = data['Last Trade'].ewm(span=20, adjust=False).mean().fillna(0)
-    data.to_csv(path_or_buf=cfg.data_file, date_format=cfg.date_format, index=False)
+    for stock in stocks:
+        data = read_data(stock_file % stock)
+        data['MA60'] = data['Last Trade'].rolling(window=60).mean().fillna(0)
+        data['MA120'] = data['Last Trade'].rolling(window=120).mean().fillna(0)
+        data['MA180'] = data['Last Trade'].rolling(window=180).mean().fillna(0)
+        data['VOL60'] = data['Last Trade'].rolling(window=60).std().fillna(0)
+        data['EMA20'] = data['Last Trade'].ewm(span=20, adjust=False).mean().fillna(0)
+        data['RSI2'] = data['Last Trade'].ewm(span=20, adjust=False).mean().fillna(0)
+        data.to_csv(path_or_buf=train_file % stock, date_format=date_format, index=False)
+
+def read_data(file):
+    date_parser = lambda x: dt.strptime(x, date_format)
+    data = pd.read_csv(file, parse_dates=[date_key], date_parser=date_parser)
     return data
 
-def plot_data(data):
-    data = pd.read_csv(cfg.data_file)
-    time_data = data[cfg.date_key]
+def plot_data(data, feature_keys):
+    colors = ["blue","orange","green","red","purple","brown","pink","gray","olive","cyan"]
+    time_data = data[date_key]
     fig, axes = plt.subplots(
         nrows=7, ncols=2, figsize=(15, 20), dpi=80, facecolor="w", edgecolor="k"
     )
-    for i in range(len(cfg.feature_keys)):
-        key = cfg.feature_keys[i]
-        c = cfg.colors[i % (len(cfg.colors))]
+    for i in range(len(feature_keys)):
+        key = feature_keys[i]
+        c = colors[i % (len(colors))]
         t_data = data[key]
         t_data.index = time_data
         t_data.head()
@@ -68,11 +76,12 @@ def plot_data(data):
             title=key,
             rot=25,
         )
-        ax.legend([cfg.feature_keys[i]])
+        ax.legend([feature_keys[i]])
     plt.tight_layout()
     plt.show()
 
 def main():
+    data_preprocessing()
     print(get_beta_difference())
 
 if __name__ == '__main__':
