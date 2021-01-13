4e26889613b747c1ead27975153d700439308f80
saxon <saxon.cs.lok@gmail.com>
2021-01-13 21:35:08 +0800

    beta done

--- test/beta_strategy.py
+++ test/beta_strategy.py
@@ -1,4 +1,5 @@
 import backtrader as bt
+from backtrader import sizer
 import util.data as dta
 from datetime import datetime
 
@@ -25,21 +26,47 @@ class BetaStrategy(bt.Strategy):
                     d.plotinfo.plotmaster = self.datas[0]
 
     def next(self):
+        # for i, d in enumerate(self.datas):
+        #     dt, dn = self.datetime.date(), d._name
+        #     pos = self.getposition(d).size
+        #     if not pos:  # no market / no orders
+        #         if self.inds[d]['cross'][0] == 1:
+        #             self.buy(data=d, size=1000)
+        #         elif self.inds[d]['cross'][0] == -1:
+        #             self.sell(data=d, size=1000)
+        #     else:
+        #         if self.inds[d]['cross'][0] == 1:
+        #             self.close(data=d)
+        #             self.buy(data=d, size=1000)
+        #         elif self.inds[d]['cross'][0] == -1:
+        #             self.close(data=d)
+        #             self.sell(data=d, size=1000)
+        if len(self.datas[0]) == self.data0.buflen():
+            return
+        long_stock_beta = self.datas[0].tick_Beta_Diff
+        long_stock = 0
+        short_stock_beta = self.datas[0].tick_Beta_Diff
+        short_stock = 0
         for i, d in enumerate(self.datas):
             dt, dn = self.datetime.date(), d._name
-            pos = self.getposition(d).size
-            if not pos:  # no market / no orders
-                if self.inds[d]['cross'][0] == 1:
-                    self.buy(data=d, size=1000)
-                elif self.inds[d]['cross'][0] == -1:
-                    self.sell(data=d, size=1000)
-            else:
-                if self.inds[d]['cross'][0] == 1:
-                    self.close(data=d)
-                    self.buy(data=d, size=1000)
-                elif self.inds[d]['cross'][0] == -1:
-                    self.close(data=d)
-                    self.sell(data=d, size=1000)
+            if d.tick_Beta_Diff > long_stock_beta:
+                long_stock = i
+                long_stock_beta = d.tick_Beta_Diff
+            if d.tick_Beta_Diff < short_stock_beta:
+                short_stock = i
+                short_stock_beta = d.tick_Beta_Diff
+
+        if long_stock == short_stock:
+            pass
+
+        if self.getposition(self.datas[short_stock]).size > 0:
+            # print(self.datas[short_stock]._name)
+            o = self.sell(data=self.datas[short_stock])
+
+        o = self.buy(data=self.datas[long_stock])
+        # print(self.datas[long_stock]._name)
+        
+        # print(self.broker.getvalue())
 
     def notify_trade(self, trade):
         dt = self.data.datetime.date()
@@ -50,7 +77,9 @@ class BetaStrategy(bt.Strategy):
                                                 round(trade.pnl,2),
                                                 round(trade.pnlcomm,2)))
 
+
 class OandaCSVData(bt.feeds.GenericCSVData):
+    lines = ('Beta_Diff',)
     params = (
         ('nullvalue', float('NaN')),
         ('dtformat', '%Y/%m/%d %H:%M'),
@@ -61,19 +90,27 @@ class OandaCSVData(bt.feeds.GenericCSVData):
         ('low', 1),
         ('close', 1),
         ('volume', 2),
-        ('openinterest', -1),
-    )
+        ('Beta_Diff', 10),
+    )    # Add a 'pe' line to the inherited ones from the base class
 
 if __name__ == '__main__':
     stocks = ['27', '200', '880', '2282']
-    startcash = 10000
+    startcash = 1000000
     cerebro = bt.Cerebro()
     cerebro.broker.setcash(startcash)
     cerebro.broker.setcommission(commission=0.001)
     cerebro.addstrategy(BetaStrategy)
+    # for stock in stocks:
+    #     data = OandaCSVData(
+    #         dataname='data/train/Equities_%s.csv' % stock, Beta_Diff=10)
+    #     cerebro.adddata(data, name=stock)
+
     for stock in stocks:
-        data = OandaCSVData(dataname='data/train/Equities_%s.csv' % stock)
+        data = OandaCSVData(dataname='data/train/Equities_%s.csv' %
+                            stock, Beta_Diff=10)
         cerebro.adddata(data, name=stock)
+    print(cerebro.broker.getvalue())
+    cerebro.addsizer(sizercls=bt.sizers.FixedSize, stake=2000)
     cerebro.run()
 
     portvalue = cerebro.broker.getvalue()
